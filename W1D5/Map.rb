9f5607c4c52162906d85f1980ebd344b3ffbde91
your name here <francis.miles.lo@gmail.com>
2018-08-24 00:19:49 -0700

    complete Map class

--- W1D5/Map.rb
+++ W1D5/Map.rb
@@ -1,16 +1,38 @@
-def Map
+class Map
   def initialize
     @array = []
   end
   def set(key, value)
+    index = array.index { |subarr| subarr[0] == key }
+    if index
+      array[index][1] = value
+    else
+      array.push([key, value])
+    end
+
+    value
   end
 
   def get(key)
+    array.each {|subarr| return subarr[1] if subarr[0] == key }
+    nil
   end
 
   def delete(key)
+    value = get(key)
+    array.reject! { |subarr| subarr[0] == key }
+    value
   end
 
   def show
+    deep_dup(array)
+  end
+
+  private
+
+  attr_reader :array
+
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
   end
 end
