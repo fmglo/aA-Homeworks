555c1d940ebe68f4e2231b0de330e7aabba09448
your name here <francis.miles.lo@gmail.com>
2018-08-23 22:42:56 -0700

    add code for enqueue, dequeue, peek

--- W1D5/Queue.rb
+++ W1D5/Queue.rb
@@ -3,12 +3,17 @@ class Queue
   def initialize
     @queue = []
   end
+
   def enqueue(el)
+    @queue.unshift(el)
   end
 
   def dequeue
+    @queue.pop
   end
 
   def peek
+    @queue[-1]
   end
+
 end
